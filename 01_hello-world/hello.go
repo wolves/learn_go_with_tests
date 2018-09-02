22f7616bed9eb91526a29062ca6bbb5480c7792f
Christopher Stingl <cjstingl@gmail.com>
2018-09-02 13:01:25 -0400

    01_completed

---
+++ 01_hello-world/hello.go
@@ -0,0 +1,33 @@
+package hello
+
+import "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+const englishPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
